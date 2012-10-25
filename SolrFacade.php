57116cc8ab430f0e7aacc67220ad413f68e13333
Florian Semm <florian.semm@gmx.de>
2012-10-25 07:19:27 +0200

    remove debugging print_r

--- SolrFacade.php
+++ SolrFacade.php
@@ -225,7 +225,7 @@ class SolrFacade {
 
 		$targetEntity = $query->getEntity();
 		$mappedEntities = array();
-		foreach ($response['response']['docs'] as $document) {print_r($document);
+		foreach ($response['response']['docs'] as $document) {
 			$mappedEntities[] = $this->entityMapper->toEntity($document, $targetEntity);
 		}
 		
