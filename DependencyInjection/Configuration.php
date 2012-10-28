2acaccfdb56053cb902f79a5c48c94aab4b563ef
Florian Semm <florian.semm@gmx.de>
2012-10-28 15:47:42 +0100

    testcommit

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -12,9 +12,7 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
  */
 class Configuration implements ConfigurationInterface
 {
-    /**
-     * {@inheritDoc}
-     */
+
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
