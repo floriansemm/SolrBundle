583db44638d0f3ab25da0bfd3bb0b04696d4575d
Florian Semm <florian.semm@gmx.de>
2012-11-16 08:20:59 +0100

    Update Tests/bootstrap.php
    debug

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -4,7 +4,7 @@ $file = __DIR__.'/../../../../../../app/bootstrap.php.cache';
 
 
 if (!file_exists($file)) {
-	throw new RuntimeException('Install dependencies to run test suite. "php composer.phar install --dev"');
+	throw new RuntimeException('Install dependencies to run test suite. "php composer.phar install --dev"'.__DIR__);
 }
 
 require_once $file;
