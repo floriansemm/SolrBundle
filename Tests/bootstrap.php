b51f8c63672e3e4737bf3278dfcfc3043b9a3105
Florian Semm <florian.semm@gmx.de>
2012-11-17 15:23:25 +0100

    Update Tests/bootstrap.php

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -2,11 +2,5 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-if (!file_exists($file)) {
-	throw new RuntimeException('Install dependencies to run test suite. "php composer.phar install --dev"'.__DIR__);
-}
-
 \Doctrine\Common\Annotations\AnnotationRegistry::registerLoader('class_exists');
 \Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver::registerAnnotationClasses();
-
-require_once $file;
