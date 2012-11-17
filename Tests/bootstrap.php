c81dc3624eb43f0b3da7767f2348bc1d6c562f78
Florian Semm <florian.semm@gmx.de>
2012-11-17 14:05:35 +0100

    register annotations

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,10 +1,16 @@
 <?php
+
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
+
+
 $file = __DIR__.'/../vendor/autoload.php';
-
-
 
 if (!file_exists($file)) {
 	throw new RuntimeException('Install dependencies to run test suite. "php composer.phar install --dev"'.__DIR__);
 }
+
+AnnotationDriver::registerAnnotationClasses();
+AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 require_once $file;
