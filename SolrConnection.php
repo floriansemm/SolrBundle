340ee4c2bc53b990605354f00ac3087483a45ec9
Florian Semm <florian.semm@gmx.de>
2013-10-24 08:26:03 +0200

    psr

--- SolrConnection.php
+++ SolrConnection.php
@@ -3,23 +3,25 @@ namespace FS\SolrBundle;
 
 use Solarium\Client;
 
-class SolrConnection {
-	
-	/**
-	 * @var array
-	 */
-	private $connection = array();
-	
-	/**
-	 * @var Solarium\Client
-	 */
-	private $client = null;
-	
-	/**
-	 * @param array $connection
-	 */
-	public function __construct(array $connection = array()) {
-		$this->connection = $connection;
+class SolrConnection
+{
+
+    /**
+     * @var array
+     */
+    private $connection = array();
+
+    /**
+     * @var Solarium\Client
+     */
+    private $client = null;
+
+    /**
+     * @param array $connection
+     */
+    public function __construct(array $connection = array())
+    {
+        $this->connection = $connection;
 
         $this->client = new Client(array(
             'endpoint' => array(
@@ -28,34 +30,37 @@ class SolrConnection {
                     'port' => 8983,
                     'path' => '/solr/',
                 )
-            )));
-	}
+            )
+        ));
+    }
 
 
-	/**
-	 * @return array
-	 */
-	public function getConnection() {
-		return $this->connection;
-	}
-	
-	/**
-	 * @throws \RuntimeException if the client cannot connect so Solr host
-	 * @return Solarium\Client
-	 */
-	public function getClient() {
-		try {
+    /**
+     * @return array
+     */
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+
+    /**
+     * @throws \RuntimeException if the client cannot connect so Solr host
+     * @return Solarium\Client
+     */
+    public function getClient()
+    {
+        try {
             $ping = $this->client->createPing();
 
-			$this->client->ping($ping);
-		} catch (\Exception $e) {
-			$host = $this->connection['hostname'];
-			$port = $this->connection['port'];
-			$path = $this->connection['path'];
-			
-			throw new \RuntimeException(sprintf('Cannot connect to Solr host: %s:%s, path: %s', $host, $port, $path));
-		}
-		
-		return $this->client;
-	}
+            $this->client->ping($ping);
+        } catch (\Exception $e) {
+            $host = $this->connection['hostname'];
+            $port = $this->connection['port'];
+            $path = $this->connection['path'];
+
+            throw new \RuntimeException(sprintf('Cannot connect to Solr host: %s:%s, path: %s', $host, $port, $path));
+        }
+
+        return $this->client;
+    }
 }
