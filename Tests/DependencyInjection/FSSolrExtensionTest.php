2efac685f306b015c5cf815b847274ecb865456e
Florian Semm <florian.semm@gmx.de>
2013-10-28 07:44:20 +0100

    fix tests after refactoring of extension

--- Tests/DependencyInjection/FSSolrExtensionTest.php
+++ Tests/DependencyInjection/FSSolrExtensionTest.php
@@ -5,6 +5,7 @@ namespace FS\SolrBundle\Tests\DependencyInjection;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use FS\SolrBundle\DependencyInjection\FSSolrExtension;
+use Symfony\Component\DependencyInjection\Reference;
 
 /**
  *
@@ -21,9 +22,16 @@ class FSSolrExtensionTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->container = new ContainerBuilder();
+    }
 
-        $definition = new Definition();
-        $this->container->setDefinition('doctrine.orm.default_configuration', $definition);
+    private function enableOdmConfig()
+    {
+        $this->container->setParameter('doctrine_mongodb.odm.document_managers', array('default'=>'odm.default.mananger'));
+    }
+
+    private function enableOrmConfig()
+    {
+        $this->container->setParameter('doctrine.entity_managers', array('default'=>'orm.default.mananger'));
     }
 
     private function commonConfig()
@@ -48,6 +56,7 @@ class FSSolrExtensionTest extends \PHPUnit_Framework_TestCase
      */
     public function solrClientsWithCommonSettings()
     {
+        $this->enableOrmConfig();
         $config = $this->commonConfig();
 
         $extension = new FSSolrExtension();
@@ -101,6 +110,7 @@ class FSSolrExtensionTest extends \PHPUnit_Framework_TestCase
                 )
             )
         ));
+        $this->enableOrmConfig();
 
         $extension = new FSSolrExtension();
         $extension->load($config, $this->container);
@@ -112,6 +122,7 @@ class FSSolrExtensionTest extends \PHPUnit_Framework_TestCase
 
     public function testDoctrineORMSetup()
     {
+        $this->enableOrmConfig();
         $config = $this->commonConfig();
 
         $extension = new FSSolrExtension();
@@ -125,18 +136,13 @@ class FSSolrExtensionTest extends \PHPUnit_Framework_TestCase
         $this->assertDefinitionHasTag('solr.delete.document.orm.listener', 'doctrine.event_listener');
         $this->assertDefinitionHasTag('solr.add.document.orm.listener', 'doctrine.event_listener');
 
-        $doctrineArguments = $this->container->getDefinition('solr.doctrine.configuration')->getArguments();
-        $arguments = array_pop($doctrineArguments);
-        $doctrineConfiguration = $arguments;
-
-        $this->assertEquals('doctrine.orm.default_configuration', $doctrineConfiguration);
+        $this->assertClassnameResolverHasOrmDefaultConfiguration();
     }
 
     public function testDoctrineODMSetup()
     {
         $config = $this->commonConfig();
-
-        $this->container->setParameter('doctrine_mongodb.odm.document_managers', true);
+        $this->enableOdmConfig();
 
         $extension = new FSSolrExtension();
         $extension->load($config, $this->container);
@@ -149,18 +155,44 @@ class FSSolrExtensionTest extends \PHPUnit_Framework_TestCase
         $this->assertDefinitionHasTag('solr.delete.document.odm.listener', 'doctrine_mongodb.odm.event_listener');
         $this->assertDefinitionHasTag('solr.add.document.odm.listener', 'doctrine_mongodb.odm.event_listener');
 
-        $doctrineArguments = $this->container->getDefinition('solr.doctrine.configuration')->getArguments();
-        $arguments = array_pop($doctrineArguments);
-        $doctrineConfiguration = $arguments;
+        $this->assertClassnameResolverHasOdmDefaultConfiguration();
+    }
+
+    private function assertClassnameResolverHasOrmDefaultConfiguration()
+    {
+        $doctrineConfiguration = $this->getReferenzIdOfCalledMethod();
+
+        $this->assertEquals('doctrine.orm.default_configuration', $doctrineConfiguration);
+    }
+
+    private function assertClassnameResolverHasOdmDefaultConfiguration()
+    {
+        $doctrineConfiguration = $this->getReferenzIdOfCalledMethod();
 
         $this->assertEquals('doctrine_mongodb.odm.default_configuration', $doctrineConfiguration);
     }
 
+    /**
+     * @return Reference
+     */
+    private function getReferenzIdOfCalledMethod()
+    {
+        $methodCalls = $this->container->getDefinition('solr.doctrine.classnameresolver')->getMethodCalls();
+
+        $firstMethodCall = $methodCalls[0];
+        $references = $firstMethodCall[1];
+        $reference = $references[0];
+
+        return $reference;
+    }
+
     private function assertDefinitionHasTag($definition, $tag)
     {
+        $tags = $this->container->getDefinition($definition)->getTags();
+
         $this->assertTrue(
             $this->container->getDefinition($definition)->hasTag($tag),
-            sprintf('%s with %s tag', $definition, $tag)
+            sprintf('%s with %s tag, has %s', $definition, $tag, print_r($tags, true))
         );
     }
 }
