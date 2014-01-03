c832e76b1e2a5a83c02d577afa0bd2efb73e33e8
Andy <andy@andywaterman.co.uk>
2014-01-03 13:55:51 +0000

    Enable disabling of wildcards in query

--- Query/SolrQuery.php
+++ Query/SolrQuery.php
@@ -22,6 +22,11 @@ class SolrQuery extends AbstractQuery
     private $useAndOperator = false;
 
     /**
+     * @var bool
+     */
+    private $useWildcards = true;
+
+    /**
      *
      * @var Solr
      */
@@ -73,6 +78,14 @@ class SolrQuery extends AbstractQuery
     public function setUseAndOperator($strict)
     {
         $this->useAndOperator = $strict;
+    }
+
+    /**
+     * @param bool $strict
+     */
+    public function setUseWildcard($strict)
+    {
+        $this->useWildcards = $strict;
     }
 
     /**
@@ -145,7 +158,13 @@ class SolrQuery extends AbstractQuery
 
         $termCount = 1;
         foreach ($this->searchTerms as $fieldName => $fieldValue) {
-            $term .= $fieldName . ':*' . $fieldValue . '*';
+
+            if ($this->useWildcards) {
+                $term .= $fieldName . ':*' . $fieldValue . '*';
+            } else {
+                $term .= $fieldName . ':' . $fieldValue;
+            }
+
             if ($termCount < count($this->searchTerms)) {
                 $term .= ' ' . $logicOperator . ' ';
             }
