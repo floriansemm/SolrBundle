26140800603ba978e155198b552633db6e65f62b
Florian Semm <florian.semm@gmx.de>
2014-01-19 20:17:04 +0100

    add unittest

---
+++ Tests/Console/ConsoleResultFactoryTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace FS\Console;
+
+
+use FS\SolrBundle\Console\ConsoleResultFactory;
+use FS\SolrBundle\Doctrine\Mapper\MetaInformation;
+use FS\SolrBundle\Event\ErrorEvent;
+use FS\SolrBundle\Event\Event;
+use FS\SolrBundle\Tests\Doctrine\Mapper\ValidTestEntity;
+
+class ConsoleResultFactoryTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @test
+     */
+    public function resultFromErrorEventContainsExceptionMessage()
+    {
+        $error = new ErrorEvent();
+        $error->setException(new \Exception('message'));
+
+        $factory = new ConsoleResultFactory();
+        $result = $factory->fromEvent($error);
+
+        $this->assertEquals('message', $result->getMessage());
+    }
+
+    /**
+     * @test
+     */
+    public function resultNotContainsIdAndEntityWhenMetaInformationNull()
+    {
+        $event = new Event(null, null, '');
+
+        $factory = new ConsoleResultFactory();
+        $result = $factory->fromEvent($event);
+
+        $this->assertEquals(null, $result->getResultId());
+        $this->assertEquals('', $result->getEntity());
+        $this->assertEquals('', $result->getMessage());
+    }
+
+    /**
+     * @test
+     */
+    public function resultFromSuccessEventContainsNoMessage()
+    {
+        $entity = new ValidTestEntity();
+        $entity->setId(1);
+
+        $metaInformation = new MetaInformation();
+        $metaInformation->setClassName('an entity');
+        $metaInformation->setEntity($entity);
+
+        $event = new Event(null, $metaInformation, '');
+
+        $factory = new ConsoleResultFactory();
+        $result = $factory->fromEvent($event);
+
+        $this->assertEquals(1, $result->getResultId());
+        $this->assertEquals('an entity', $result->getEntity());
+        $this->assertEquals('', $result->getMessage());
+    }
+}
+ 
