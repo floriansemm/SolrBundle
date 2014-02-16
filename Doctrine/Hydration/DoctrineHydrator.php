4648c7afc61ce31b906fa9727bf25c4ad01baab1
Florian Semm <florian.semm@gmx.de>
2014-02-16 20:21:42 +0100

    add hydrator

--- Doctrine/Hydration/DoctrineHydrator.php
+++ Doctrine/Hydration/DoctrineHydrator.php
@@ -6,6 +6,9 @@ namespace FS\SolrBundle\Doctrine\Hydration;
 use FS\SolrBundle\Doctrine\Mapper\MetaInformation;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
+/**
+ * hydrates full Entity from DB and merge with result from IndexHydrator
+ */
 class DoctrineHydrator
 {
 
@@ -16,13 +19,10 @@ class DoctrineHydrator
 
     public function __construct(RegistryInterface $doctrine)
     {
-
-
         $this->doctrine;
     }
 
     public function hydrate($document)
     {
-        $foo = $document;
     }
 } 
---
+++ Doctrine/Hydration/IndexHydrator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace FS\SolrBundle\Doctrine\Hydration;
+
+/**
+ * hydrates Entity from Document
+ */
+class IndexHydrator
+{
+
+} 
