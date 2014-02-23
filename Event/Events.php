e7382523102215a71cc766848457cf8f583428b4
floriansemm <floriansemm@gmail.com>
2014-02-23 20:04:41 +0100

    rename to meaningful names

--- Event/Events.php
+++ Event/Events.php
@@ -4,17 +4,17 @@ namespace FS\SolrBundle\Event;
 
 final class Events
 {
-    const PRE_INSERT = 'pre_insert';
-    const POST_INSERT = 'post_insert';
+    const PRE_INSERT = 'solr.pre_insert';
+    const POST_INSERT = 'solr.post_insert';
 
-    const PRE_UPDATE = 'pre_update';
-    const POST_UPDATE = 'post_update';
+    const PRE_UPDATE = 'solr.pre_update';
+    const POST_UPDATE = 'solr.post_update';
 
-    const PRE_DELETE = 'pre_delete';
-    const POST_DELETE = 'post_delete';
+    const PRE_DELETE = 'solr.pre_delete';
+    const POST_DELETE = 'solr.post_delete';
 
-    const PRE_CLEAR_INDEX = 'pre_clear_index';
-    const POST_CLEAR_INDEX = 'post_clear_index';
+    const PRE_CLEAR_INDEX = 'solr.pre_clear_index';
+    const POST_CLEAR_INDEX = 'solr.post_clear_index';
 
-    const ERROR = 'error';
+    const ERROR = 'solr.error';
 } 
