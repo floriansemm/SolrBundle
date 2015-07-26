00b6fd2044fcc54fe1110762d9542f4abf630302
floriansemm <floriansemm@gmail.com>
2015-07-26 12:10:47 +0200

    move solr-setup to dedicated setup-feature-context

--- Tests/Integration/Bootstrap/FeatureContext.php
+++ Tests/Integration/Bootstrap/FeatureContext.php
@@ -8,145 +8,8 @@ use Solarium\QueryType\Update\Query\Document\Document;
 /**
  * Features context.
  */
-class FeatureContext implements Context
+class FeatureContext extends SolrSetupFeatureContext
 {
-
-    /**
-     * @var \FS\SolrBundle\Tests\Integration\EventDispatcherFake
-     */
-    private $eventDispatcher;
-
-    /**
-     * @var \Solarium\Client
-     */
-    private $solrClient;
-
-    public function __construct()
-    {
-        $autoload = __DIR__ . '/../vendor/autoload.php';
-        if (file_exists($autoload)) {
-            require_once $autoload;
-        } else {
-            require_once 'vendor/autoload.php';
-        }
-
-        $this->eventDispatcher = new \FS\SolrBundle\Tests\Integration\EventDispatcherFake();
-    }
-
-    /**
-     * @return \FS\SolrBundle\Tests\Integration\EventDispatcherFake
-     */
-    public function getEventDispatcher()
-    {
-        return $this->eventDispatcher;
-    }
-
-    /**
-     * @return \Solarium\Client
-     */
-    public function getSolrClient()
-    {
-        return $this->solrClient;
-    }
-
-    /**
-     * @return \FS\SolrBundle\Solr
-     */
-    public function getSolrInstance()
-    {
-        \Doctrine\Common\Annotations\AnnotationRegistry::registerLoader('class_exists');
-        \Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver::registerAnnotationClasses();
-
-        $this->solrClient = $this->setupSolrClient();
-        $factory = $this->setupCommandFactory();
-        $metaFactory = $this->setupMetaInformationFactory();
-        $entityMapper = $this->setupEntityMapper();
-
-        $solr = new \FS\SolrBundle\Solr(
-            $this->solrClient,
-            $factory,
-            $this->eventDispatcher,
-            $metaFactory,
-            $entityMapper
-        );
-
-        return $solr;
-    }
-
-    /**
-     * @return \FS\SolrBundle\Doctrine\Mapper\EntityMapper
-     */
-    private function setupEntityMapper()
-    {
-        $registry = new \FS\SolrBundle\Tests\Integration\DoctrineRegistryFake();
-
-        $entityMapper = new \FS\SolrBundle\Doctrine\Mapper\EntityMapper(
-            new \FS\SolrBundle\Doctrine\Hydration\DoctrineHydrator(
-                $registry,
-                new \FS\SolrBundle\Doctrine\Hydration\ValueHydrator()
-            ),
-            new \FS\SolrBundle\Doctrine\Hydration\IndexHydrator(
-                new \FS\SolrBundle\Doctrine\Hydration\ValueHydrator()
-            )
-        );
-
-        return $entityMapper;
-    }
-
-    /**
-     * @return \FS\SolrBundle\Doctrine\Mapper\Mapping\CommandFactory
-     */
-    private function setupCommandFactory()
-    {
-        $factory = new \FS\SolrBundle\Doctrine\Mapper\Mapping\CommandFactory();
-        $factory->add(new \FS\SolrBundle\Doctrine\Mapper\Mapping\MapAllFieldsCommand(), 'all');
-        $factory->add(new \FS\SolrBundle\Doctrine\Mapper\Mapping\MapIdentifierCommand(), 'identifier');
-
-        return $factory;
-    }
-
-    /**
-     * @return \FS\SolrBundle\Doctrine\Mapper\MetaInformationFactory
-     */
-    private function setupMetaInformationFactory()
-    {
-        $ormConfiguration = new \Doctrine\ORM\Configuration();
-        $ormConfiguration->addEntityNamespace('FSTest:ValidTestEntity', 'FS\SolrBundle\Tests\Doctrine\Mapper');
-
-        $knowNamespaces = new \FS\SolrBundle\Doctrine\ClassnameResolver\KnownNamespaceAliases();
-        $knowNamespaces->addEntityNamespaces($ormConfiguration);
-
-        $classnameResolver = new \FS\SolrBundle\Doctrine\ClassnameResolver\ClassnameResolver($knowNamespaces);
-
-        $metaFactory = new \FS\SolrBundle\Doctrine\Mapper\MetaInformationFactory();
-        $metaFactory->setClassnameResolver(
-            $classnameResolver
-        );
-
-        return $metaFactory;
-    }
-
-    /**
-     * Solarium Client with one core (core0)
-     *
-     * @return \Solarium\Client
-     */
-    private function setupSolrClient()
-    {
-        $config = array(
-            'default' => array(
-                'host' => 'localhost',
-                'port' => 8983,
-                'path' => '/solr/core0',
-            )
-        );
-
-        $builder = new \FS\SolrBundle\Client\SolrBuilder($config);
-        $solrClient = $builder->build();
-
-        return $solrClient;
-    }
-
     /**
      * @param int    $entityId
      * @param string $documentName
@@ -155,8 +18,8 @@ class FeatureContext implements Context
      */
     public function assertInsertSuccessful($entityId, $documentName)
     {
-        if (!$this->eventDispatcher->eventOccurred(\FS\SolrBundle\Event\Events::POST_INSERT) ||
-            !$this->eventDispatcher->eventOccurred(\FS\SolrBundle\Event\Events::PRE_INSERT)
+        if (!$this->getEventDispatcher()->eventOccurred(\FS\SolrBundle\Event\Events::POST_INSERT) ||
+            !$this->getEventDispatcher()->eventOccurred(\FS\SolrBundle\Event\Events::PRE_INSERT)
         ) {
             throw new \RuntimeException('Insert was not successful');
         }
@@ -169,6 +32,13 @@ class FeatureContext implements Context
         }
     }
 
+    /**
+     * Field value documentname_1 becomes 1
+     *
+     * @param string $keyField
+     *
+     * @return string
+     */
     private function removeKeyFieldNameSuffix($keyField)
     {
         return substr($keyField, strpos($keyField, '_') + 1);
